727188ed72523f23abd2c88cbb3eb543d4baa9c3
abdull-one <abdullahijangadeQ@gmail.com>
2025-07-19 07:46:09 +0100

    Initial React app with WelcomeMessage component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,16 +1,15 @@
-import WelcomeMessage from './WelcomeMessage.jsx';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import WelcomeMessage from './components/WelcomeMessage';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <WelcomeMessage />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,11 +26,15 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
+      
+      <WelcomeMessage />
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+// src/WelcomeMessage.jsx
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Hello everyone I'm learning REACT with ALX!</h1>
+      <p>This is a simple JSX component.</p>
+      <p>I'm learning ABOUT JSX!</p> 
+    </div>
+  );
+}
+
+export default WelcomeMessage;
