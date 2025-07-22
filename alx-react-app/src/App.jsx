bd270daccbd0ff3699a6313b01cf4cc7cd9e2d34
abdull-one <abdullahijangadeQ@gmail.com>
2025-07-22 15:40:02 +0100

    Add UserProfile component with props and render in App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,16 @@
+import WelcomeMessage from './components/WelcomeMessage';
 import Header from './Header';
 import MainContent from './MainContent';  
 import Footer from './Footer';
-import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 function App() {
   return (
     <>
+    <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography." />
       <Footer />
-      <WelcomeMessage />
     </>
   );
 }
