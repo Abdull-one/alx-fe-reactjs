5cad6fa302732b6c1cc016c60d5b450853853748
abdull-one <abdullahijangadeQ@gmail.com>
2025-07-28 19:02:47 +0100

    Initialize recipe-sharing-app with Zustand store and components

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-xl font-bold mb-4">Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
