24d697c610db6fae1be6635e0597ab831a63f334
abdull-one <abdullahijangadeQ@gmail.com>
2025-07-30 17:00:17 +0100

    Final cleanup before deployment

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,8 +3,8 @@ import Search from './components/Search';
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-800">
-      <h1 className="text-green-500 text-2xl font-bold">Tailwind is now working!</h1>
-      <h1 className="text-center text-3xl font-bold mt-6">GitHub User Search</h1>
+      <h1 className="text-green-500 text-2xl font-bold">Welcome to github user services platform!</h1>
+      <h2 className="text-center text-3xl font-bold mt-6">GitHub User Search</h2>
       <Search />
     </div>
   );
