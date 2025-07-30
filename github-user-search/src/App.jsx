99e31ca433761e69732821ddbe8f017efbeaccdb
abdull-one <abdullahijangadeQ@gmail.com>
2025-07-30 16:34:37 +0100

    Setup Tailwind CSS with advanced search UI

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/Search';
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-800">
+      <h1 className="text-green-500 text-2xl font-bold">Tailwind is now working!</h1>
       <h1 className="text-center text-3xl font-bold mt-6">GitHub User Search</h1>
       <Search />
     </div>
